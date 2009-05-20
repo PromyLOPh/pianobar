a3a12a524c41f45d9cd9ebaa94cb22dc1456e6b1
Lars-Dominik Braun <PromyLOPh@lavabit.com>
2009-05-20 17:24:33 +0200

    piano: Protocol version bump
    v24 now; blowfish key stays the same; no imcompatible changes found yet

--- libpiano/src/main.c
+++ libpiano/src/main.c
@@ -33,7 +33,7 @@ THE SOFTWARE.
 #include "crypt.h"
 #include "config.h"
 
-#define PIANO_PROTOCOL_VERSION "23"
+#define PIANO_PROTOCOL_VERSION "24"
 #define PIANO_RPC_HOST "www.pandora.com"
 #define PIANO_RPC_PORT "80"
 #define PIANO_RPC_PATH "/radio/xmlrpc/v" PIANO_PROTOCOL_VERSION "?"
