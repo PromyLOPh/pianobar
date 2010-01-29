d38167c27597a8a114fde0f1fef2a73b3277ba36
Lars-Dominik Braun <PromyLOPh@lavabit.com>
2010-01-29 09:59:11 +0100

    waitress: Increase tcp receive buffer

--- libwaitress/src/waitress.c
+++ libwaitress/src/waitress.c
@@ -334,6 +334,9 @@ WaitressReturn_t WaitressFetchCall (WaitressHandle_t *waith) {
 	sockpoll.fd = sockfd;
 
 	fcntl (sockfd, F_SETFL, O_NONBLOCK);
+	/* increase socket receive buffer */
+	const int sockopt = 256*1024;
+	setsockopt (sockfd, SOL_SOCKET, SO_RCVBUF, &sockopt, sizeof (sockopt));
 
 	/* non-blocking connect will return immediately */
 	connect (sockfd, res->ai_addr, res->ai_addrlen);
