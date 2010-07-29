df4022ccda870f2a1b0e4e254f4a298da48fc4f9
Lars-Dominik Braun <PromyLOPh@lavabit.com>
2010-07-29 14:12:20 +0200

    Useless comparison removed
    Unsigned int is always >= 0.

--- src/ui_readline.c
+++ src/ui_readline.c
@@ -100,13 +100,11 @@ size_t BarReadline (char *buf, size_t bufSize, const char *mask,
 					} else {
 						/* delete utf-8 multibyte chars */
 						/* char content */
-						while (bufPos >= 0 &&
-								BarReadlineIsUtf8Content (buf[bufPos-1])) {
+						while (BarReadlineIsUtf8Content (buf[bufPos-1])) {
 							BarReadlineMoveLeft (buf, &bufPos, &bufLen);
 						}
 						/* char length */
-						if (bufPos >= 0 &&
-								BarReadlineIsUtf8Start (buf[bufPos-1])) {
+						if (BarReadlineIsUtf8Start (buf[bufPos-1])) {
 							BarReadlineMoveLeft (buf, &bufPos, &bufLen);
 						}
 					}
