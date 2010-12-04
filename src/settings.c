PromyLOPh/pianobar

commit 2b31c97de6fa541719cd26f9a61053cfc0e71203

Show custom rating icon in upcoming/history song list
Closes issue #54

src/settings.c
@@ -77,6 +77,8 @@ void BarSettingsDestroy (BarSettings_t *settings) {
 	free (settings->password);
 	free (settings->autostartStation);
 	free (settings->eventCmd);
+	free (settings->loveIcon);
+	free (settings->banIcon);
 	memset (settings, 0, sizeof (*settings));
 }
 
@@ -175,6 +177,10 @@ void BarSettingsRead (BarSettings_t *settings) {
 					break;
 				}
 			}
+		} else if (strcmp ("love_icon", key) == 0) {
+			settings->loveIcon = strdup (val);
+		} else if (strcmp ("ban_icon", key) == 0) {
+			settings->banIcon = strdup (val);
 		}
 	}
 
@@ -184,6 +190,14 @@ void BarSettingsRead (BarSettings_t *settings) {
 		if (tmpProxy != NULL && strlen (tmpProxy) > 0) {
 			settings->proxy = strdup (tmpProxy);
 		}
+	}
+
+	/* use default strings */
+	if (settings->loveIcon == NULL) {
+		settings->loveIcon = strdup ("<3");
+	}
+	if (settings->banIcon == NULL) {
+		settings->banIcon = strdup ("</3");
 	}
 
 	fclose (configfd);

contrib/config-example
@@ -36,4 +36,5 @@
 #autostart_station = 123456
 #event_command = /home/user/.config/pianobar/eventcmd
 #sort = quickmix_10_name_az
-
+#love_icon = [+]
+#ban_icon = [-]

src/main.c
@@ -259,7 +259,8 @@ int main (int argc, char **argv) {
 				}
 				/* song ready to play */
 				if (app.playlist != NULL) {
-					BarUiPrintSong (app.playlist, app.curStation->isQuickMix ?
+					BarUiPrintSong (&app.settings, app.playlist,
+							app.curStation->isQuickMix ?
 							PianoFindStationById (app.ph.stations,
 							app.playlist->stationId) : NULL);
 

src/pianobar.1
@@ -140,6 +140,10 @@ or the key you defined in
 .B act_songinfo.
 
 .TP
+.B ban_icon = </3
+Icon for banned songs.
+
+.TP
 .B control_proxy = http://host:port/
 Non-american users need a proxy to use pandora.com. Only the xmlrpc interface
 will use this proxy. The music is streamed directly.
@@ -152,6 +156,10 @@ File that is executed when event occurs. See section
 .TP
 .B history = 5
 Keep a history of the last n songs (5, by default). You can rate these songs.
+
+.TP
+.B love_icon = <3
+Icon for loved songs.
 
 .TP
 .B password = plaintext_password

src/settings.h
@@ -77,6 +77,8 @@ typedef struct {
 	char keys[BAR_KS_COUNT];
 	char *autostartStation;
 	char *eventCmd;
+	char *loveIcon;
+	char *banIcon;
 } BarSettings_t;
 
 void BarSettingsInit (BarSettings_t *);

src/ui.c
@@ -330,27 +330,24 @@ PianoStation_t *BarUiSelectStation (PianoHandle_t *ph, const char *prompt,
  *	@param song list
  *	@return pointer to selected item in song list or NULL
  */
-PianoSong_t *BarUiSelectSong (PianoSong_t *startSong, FILE *curFd) {
+PianoSong_t *BarUiSelectSong (const BarSettings_t *settings,
+		PianoSong_t *startSong, FILE *curFd) {
 	PianoSong_t *tmpSong = NULL;
 	int i = 0;
 
-	/* print all songs */
-	tmpSong = startSong;
-	while (tmpSong != NULL) {
-		BarUiMsg (MSG_LIST, "%2u) %s - %s\n", i, tmpSong->artist,
-				tmpSong->title);
-		i++;
-		tmpSong = tmpSong->next;
-	}
+	i = BarUiListSongs (settings, startSong);
+
 	BarUiMsg (MSG_QUESTION, "Select song: ");
 	if (BarReadlineInt (&i, curFd) == 0) {
 		return NULL;
 	}
+
 	tmpSong = startSong;
 	while (tmpSong != NULL && i > 0) {
 		tmpSong = tmpSong->next;
 		i--;
 	}
+
 	return tmpSong;
 }
 
@@ -436,14 +433,16 @@ char *BarUiSelectMusicId (BarApp_t *app, FILE *curFd, char *similarToId) {
 					musicId = strdup (tmpArtist->musicId);
 				}
 			} else if (*selectBuf == 't') {
-				tmpSong = BarUiSelectSong (searchResult.songs, curFd);
+				tmpSong = BarUiSelectSong (&app->settings, searchResult.songs,
+						curFd);
 				if (tmpSong != NULL) {
 					musicId = strdup (tmpSong->musicId);
 				}
 			}
 		} else if (searchResult.songs != NULL) {
 			/* songs found */
-			tmpSong = BarUiSelectSong (searchResult.songs, curFd);
+			tmpSong = BarUiSelectSong (&app->settings, searchResult.songs,
+					curFd);
 			if (tmpSong != NULL) {
 				musicId = strdup (tmpSong->musicId);
 			}
@@ -539,12 +538,35 @@ inline void BarUiPrintStation (PianoStation_t *station) {
  *	@param the song
  *	@param alternative station info (show real station for quickmix, e.g.)
  */
-inline void BarUiPrintSong (PianoSong_t *song, PianoStation_t *station) {
-	BarUiMsg (MSG_PLAYING, "\"%s\" by \"%s\" on \"%s\"%s%s%s\n",
+inline void BarUiPrintSong (const BarSettings_t *settings,
+		const PianoSong_t *song, const PianoStation_t *station) {
+	BarUiMsg (MSG_PLAYING, "\"%s\" by \"%s\" on \"%s\"%s%s%s%s\n",
 			song->title, song->artist, song->album,
-			(song->rating == PIANO_RATE_LOVE) ? " <3" : "",
+			(song->rating == PIANO_RATE_LOVE) ? " " : "",
+			(song->rating == PIANO_RATE_LOVE) ? settings->loveIcon : "",
 			station != NULL ? " @ " : "",
 			station != NULL ? station->name : "");
+}
+
+/*	Print list of songs
+ *	@param pianobar settings
+ *	@param linked list of songs
+ *	@return # of songs
+ */
+size_t BarUiListSongs (const BarSettings_t *settings,
+		const PianoSong_t *song) {
+	size_t i = 0;
+
+	while (song != NULL) {
+		BarUiMsg (MSG_LIST, "%2lu) %s - %s %s%s\n", i, song->artist,
+				song->title,
+				(song->rating == PIANO_RATE_LOVE) ? settings->loveIcon : "",
+				(song->rating == PIANO_RATE_BAN) ? settings->banIcon : "");
+		song = song->next;
+		i++;
+	}
+
+	return i;
 }
 
 /*	Excute external event handler

src/ui.h
@@ -38,12 +38,14 @@ void BarUiMsg (uiMsg_t type, const char *format, ...);
 PianoReturn_t BarUiPrintPianoStatus (PianoReturn_t ret);
 PianoStation_t *BarUiSelectStation (PianoHandle_t *, const char *,
 		BarStationSorting_t, FILE *);
-PianoSong_t *BarUiSelectSong (PianoSong_t *startSong, FILE *curFd);
+PianoSong_t *BarUiSelectSong (const BarSettings_t *, PianoSong_t *, FILE *);
 PianoArtist_t *BarUiSelectArtist (PianoArtist_t *startArtist, FILE *curFd);
 char *BarUiSelectMusicId (BarApp_t *, FILE *, char *);
 void BarStationFromGenre (BarApp_t *, FILE *);
 void BarUiPrintStation (PianoStation_t *);
-void BarUiPrintSong (PianoSong_t *, PianoStation_t *);
+void BarUiPrintSong (const BarSettings_t *, const PianoSong_t *, 
+		const PianoStation_t *);
+size_t BarUiListSongs (const BarSettings_t *, const PianoSong_t *);
 void BarUiStartEventCmd (const BarSettings_t *, const char *,
 		const PianoStation_t *, const PianoSong_t *, const struct audioPlayer *,
 		PianoReturn_t, WaitressReturn_t);

src/ui_act.c
@@ -254,7 +254,8 @@ BarUiActCallback(BarUiActSongInfo) {
 
 	BarUiPrintStation (app->curStation);
 	/* print real station if quickmix */
-	BarUiPrintSong (app->playlist, app->curStation->isQuickMix ?
+	BarUiPrintSong (&app->settings, app->playlist,
+			app->curStation->isQuickMix ?
 			PianoFindStationById (app->ph.stations, app->playlist->stationId) :
 			NULL);
 }
@@ -420,13 +421,7 @@ BarUiActCallback(BarUiActPrintUpcoming) {
 
 	PianoSong_t *nextSong = app->playlist->next;
 	if (nextSong != NULL) {
-		int i = 0;
-		while (nextSong != NULL) {
-			BarUiMsg (MSG_LIST, "%2i) \"%s\" by \"%s\"\n", i, nextSong->title,
-					nextSong->artist);
-			nextSong = nextSong->next;
-			i++;
-		}
+		BarUiListSongs (&app->settings, nextSong);
 	} else {
 		BarUiMsg (MSG_INFO, "No songs in queue.\n");
 	}
@@ -472,7 +467,8 @@ BarUiActCallback(BarUiActHistory) {
 	PianoSong_t *selectedSong;
 
 	if (app->songHistory != NULL) {
-		selectedSong = BarUiSelectSong (app->songHistory, curFd);
+		selectedSong = BarUiSelectSong (&app->settings, app->songHistory,
+				curFd);
 		if (selectedSong != NULL) {
 			/* use user-defined keybindings */
 			allowedBuf[0] = app->settings.keys[BAR_KS_LOVE];

commit a6340288f8259cc96b5af05bce3612dbf4f9b7e4

Show default icons if config file is missing.
Bug introduced by 2b31c97de6fa541719cd26f9a61053cfc0e71203. "(null)" was
shown instead of "<3" and "</3".

src/settings.c
@@ -118,6 +118,8 @@ void BarSettingsRead (BarSettings_t *settings) {
 	settings->history = 5;
 	settings->sortOrder = BAR_SORT_NAME_AZ;
 	memcpy (settings->keys, defaultKeys, sizeof (defaultKeys));
+	settings->loveIcon = strdup ("<3");
+	settings->banIcon = strdup ("</3");
 
 	BarGetXdgConfigDir (PACKAGE "/config", configfile, sizeof (configfile));
 	if ((configfd = fopen (configfile, "r")) == NULL) {
@@ -180,8 +182,10 @@ void BarSettingsRead (BarSettings_t *settings) {
 				}
 			}
 		} else if (streq ("love_icon", key)) {
+			free (settings->loveIcon);
 			settings->loveIcon = strdup (val);
 		} else if (streq ("ban_icon", key)) {
+			free (settings->banIcon);
 			settings->banIcon = strdup (val);
 		}
 	}
@@ -192,14 +196,6 @@ void BarSettingsRead (BarSettings_t *settings) {
 		if (tmpProxy != NULL && strlen (tmpProxy) > 0) {
 			settings->proxy = strdup (tmpProxy);
 		}
-	}
-
-	/* use default strings */
-	if (settings->loveIcon == NULL) {
-		settings->loveIcon = strdup ("<3");
-	}
-	if (settings->banIcon == NULL) {
-		settings->banIcon = strdup ("</3");
 	}
 
 	fclose (configfd);