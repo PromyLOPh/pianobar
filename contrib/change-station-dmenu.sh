4b0b6646ce5294569577d231161b268fddc9c7bd
Juan C. Muller <jcmuller@gmail.com>
2011-01-19 14:39:30 -0500

    Use dmenu to change the station.

---
+++ contrib/change-station-dmenu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# For this to work, whatever pianobar is using as event_command needs to do
+# something like:
+# print $sfh "$1. $2\n" if (/station(\d+)=(.+)$/);
+# into /tmp/pianobar_stations
+
+STATION=$(cat /tmp/pianobar_stations | \
+	dmenu -nf '#888888' -nb '#222222' -sf '#ffffff' -i \
+	-sb '#285577' -p 'run application:' -fn 'Terminus 8' | \
+	sed -e 's/\([0-9]\+\)\..*/\1/')
+
+
+[[ -n $STATION ]] && echo "s$STATION" > ~/.config/pianobar/ctl
+
