2f1f63c1f342ee281da4edd8d610939719312b52
Juan C. Muller <jcmuller@gmail.com>
2011-01-20 11:09:43 -0500

    Add copyright, license and change instructions to change-station-dmenu.sh

--- contrib/change-station-dmenu.sh
+++ contrib/change-station-dmenu.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
-# For this to work, whatever pianobar is using as event_command needs to do
-# something like:
-# print $sfh "$1. $2\n" if (/station(\d+)=(.+)$/);
-# into /tmp/pianobar_stations
+# Copyright (c) 2011
+# Juan C. Muller <jcmuller@gmail.com>
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+# Use contrib/eventcmd.pl to capture station names
 
 STATION=$(cat /tmp/pianobar_stations | \
 	dmenu -nf '#888888' -nb '#222222' -sf '#ffffff' -i \
