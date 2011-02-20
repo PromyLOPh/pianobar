001bab94bba94766f22a0fab5107bbd51321b369
Lars-Dominik Braun <lars@6xq.net>
2011-02-20 10:30:05 +0100

    Update eventcmd example

--- contrib/eventcmd-examples/eventcmd.sh
+++ contrib/eventcmd-examples/eventcmd.sh
@@ -5,7 +5,7 @@ while read L; do
 	k="`echo "$L" | cut -d '=' -f 1`"
 	v="`echo "$L" | cut -d '=' -f 2`"
 	export "$k=$v"
-done < <(grep -e '^\(title\|artist\|album\|stationName\|pRet\|pRetStr\|wRet\|wRetStr\|songDuration\|songPlayed\|rating\|coverArt\|stationCount\|station[0-9]*\)=' /dev/stdin) # don't overwrite $1...
+done < <(grep -e '^\(title\|artist\|album\|stationName\|songStationName\|pRet\|pRetStr\|wRet\|wRetStr\|songDuration\|songPlayed\|rating\|coverArt\|stationCount\|station[0-9]*\)=' /dev/stdin) # don't overwrite $1...
 
 case "$1" in
 #	songstart)
