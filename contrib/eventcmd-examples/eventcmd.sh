7956d1a2f83bcb0d82da6af34a621709a21407cb
Lars-Dominik Braun <lars@6xq.net>
2011-02-27 16:42:58 +0100

    kdialog example eventcmd
    Merged Richard's kde4 example eventcmd into eventcmd.sh

--- contrib/eventcmd-examples/eventcmd.sh
+++ contrib/eventcmd-examples/eventcmd.sh
@@ -10,7 +10,15 @@ done < <(grep -e '^\(title\|artist\|album\|stationName\|songStationName\|pRet\|p
 case "$1" in
 #	songstart)
 #		echo 'naughty.notify({title = "pianobar", text = "Now playing: ' "$title" ' by ' "$artist" '"})' | awesome-client -
+
 #		echo "$title -- $artist" > $HOME/.config/pianobar/nowplaying
+
+#		if [ "$rating" -eq 1 ]
+#		then
+#			kdialog --title pianobar --passivepopup "'$title' by '$artist' on '$album' - LOVED" 10
+#		else
+#			kdialog --title pianobar --passivepopup "'$title' by '$artist' on '$album'" 10
+#		fi
 #		# or whatever you like...
 #		;;
 
@@ -27,11 +35,33 @@ case "$1" in
 #		fi
 #		;;
 
+#	songlove)
+#		kdialog --title pianobar --passivepopup "LOVING '$title' by '$artist' on '$album' on station '$stationName'" 10
+#		;;
+
+#	songshelf)
+#		kdialog --title pianobar --passivepopup "SHELVING '$title' by '$artist' on '$album' on station '$stationName'" 10
+#		;;
+
+#	songban)
+#		kdialog --title pianobar --passivepopup "BANNING '$title' by '$artist' on '$album' on station '$stationName'" 10
+#		;;
+
+#	songbookmark)
+#		kdialog --title pianobar --passivepopup "BOOKMARKING '$title' by '$artist' on '$album'" 10
+#		;;
+
+#	artistbookmark)
+#		kdialog --title pianobar --passivepopup "BOOKMARKING '$artist'" 10
+#		;;
+
 	*)
 		if [ "$pRet" -ne 1 ]; then
 			echo "naughty.notify({title = \"pianobar\", text = \"$1 failed: $pRetStr\"})" | awesome-client -
+#			kdialog --title pianobar --passivepopup "$1 failed: $pRetStr"
 		elif [ "$wRet" -ne 1 ]; then
 			echo "naughty.notify({title = \"pianobar\", text = \"$1 failed: Network error: $wRetStr\"})" | awesome-client -
+#			kdialog --title pianobar --passivepopup "$1 failed: Network error: $wRetStr"
 		fi
 		;;
 esac
