1fccbe8390a64c7a9adce29d77679c3432d804b6
Mark Szymanski <mrman208@me.com>
2011-04-16 15:36:12 -0500

    Changed \e to \033 to fix warnings.

--- src/colors.h
+++ src/colors.h
@@ -1,36 +1,36 @@
 /* Define colors used in colorized output */
 
-#define TXTBLK "\e[0;30m" // Black - Regular
-#define TXTRED "\e[0;31m" // Red
-#define TXTGRN "\e[0;32m" // Green
-#define TXTYLW "\e[0;33m" // Yellow
-#define TXTBLU "\e[0;34m" // Blue
-#define TXTPUR "\e[0;35m" // Purple
-#define TXTCYN "\e[0;36m" // Cyan
-#define TXTWHT "\e[0;37m" // White
-#define BLDBLK "\e[1;30m" // Black - Bold
-#define BLDRED "\e[1;31m" // Red
-#define BLDGRN "\e[1;32m" // Green
-#define BLDYLW "\e[1;33m" // Yellow
-#define BLDBLU "\e[1;34m" // Blue
-#define BLDPUR "\e[1;35m" // Purple
-#define BLDCYN "\e[1;36m" // Cyan
-#define BLDWHT "\e[1;37m" // White
-#define UNKBLK "\e[4;30m" // Black - Underline
-#define UNDRED "\e[4;31m" // Red
-#define UNDGRN "\e[4;32m" // Green
-#define UNDYLW "\e[4;33m" // Yellow
-#define UNDBLU "\e[4;34m" // Blue
-#define UNDPUR "\e[4;35m" // Purple
-#define UNDCYN "\e[4;36m" // Cyan
-#define UNDWHT "\e[4;37m" // White
-#define BAKBLK "\e[40m"   // Black - Background
-#define BAKRED "\e[41m"   // Red
-#define BADGRN "\e[42m"   // Green
-#define BAKYLW "\e[43m"   // Yellow
-#define BAKBLU "\e[44m"   // Blue
-#define BAKPUR "\e[45m"   // Purple
-#define BAKCYN "\e[46m"   // Cyan
-#define BAKWHT "\e[47m"   // White
-#define TXTRST "\e[0m"    // Text Reset
+#define TXTBLK "\033[0;30m" // Black - Regular
+#define TXTRED "\033[0;31m" // Red
+#define TXTGRN "\033[0;32m" // Green
+#define TXTYLW "\033[0;33m" // Yellow
+#define TXTBLU "\033[0;34m" // Blue
+#define TXTPUR "\033[0;35m" // Purple
+#define TXTCYN "\033[0;36m" // Cyan
+#define TXTWHT "\033[0;37m" // White
+#define BLDBLK "\033[1;30m" // Black - Bold
+#define BLDRED "\033[1;31m" // Red
+#define BLDGRN "\033[1;32m" // Green
+#define BLDYLW "\033[1;33m" // Yellow
+#define BLDBLU "\033[1;34m" // Blue
+#define BLDPUR "\033[1;35m" // Purple
+#define BLDCYN "\033[1;36m" // Cyan
+#define BLDWHT "\033[1;37m" // White
+#define UNKBLK "\033[4;30m" // Black - Underline
+#define UNDRED "\033[4;31m" // Red
+#define UNDGRN "\033[4;32m" // Green
+#define UNDYLW "\033[4;33m" // Yellow
+#define UNDBLU "\033[4;34m" // Blue
+#define UNDPUR "\033[4;35m" // Purple
+#define UNDCYN "\033[4;36m" // Cyan
+#define UNDWHT "\033[4;37m" // White
+#define BAKBLK "\033[40m"   // Black - Background
+#define BAKRED "\033[41m"   // Red
+#define BADGRN "\033[42m"   // Green
+#define BAKYLW "\033[43m"   // Yellow
+#define BAKBLU "\033[44m"   // Blue
+#define BAKPUR "\033[45m"   // Purple
+#define BAKCYN "\033[46m"   // Cyan
+#define BAKWHT "\033[47m"   // White
+#define TXTRST "\033[0m"    // Text Reset
 
