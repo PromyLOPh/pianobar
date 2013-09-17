9543e51c7b545311f675f368ada0aacdab8c067d
1800alex <1800alex@gmail.com>
2013-09-17 09:53:22 -0400

    Update ui_act.c
    Added support for sending event on pause/play.

--- src/ui_act.c
+++ src/ui_act.c
@@ -413,6 +413,10 @@ BarUiActCallback(BarUiActPlay) {
 	app->player.doPause = false;
 	pthread_cond_broadcast (&app->player.pauseCond);
 	pthread_mutex_unlock (&app->player.pauseMutex);
+	
+	BarUiStartEventCmd (&app->settings, "songplay",
+			app->curStation, app->playlist, &app->player, app->ph.stations,
+			PIANO_RET_OK, WAITRESS_RET_OK);
 }
 
 /*	pause
@@ -422,6 +426,10 @@ BarUiActCallback(BarUiActPause) {
 	app->player.doPause = true;
 	pthread_cond_broadcast (&app->player.pauseCond);
 	pthread_mutex_unlock (&app->player.pauseMutex);
+	
+	BarUiStartEventCmd (&app->settings, "songpause",
+			app->curStation, app->playlist, &app->player, app->ph.stations,
+			PIANO_RET_OK, WAITRESS_RET_OK);
 }
 
 /*	toggle pause
@@ -429,6 +437,20 @@ BarUiActCallback(BarUiActPause) {
 BarUiActCallback(BarUiActTogglePause) {
 	pthread_mutex_lock (&app->player.pauseMutex);
 	app->player.doPause = !app->player.doPause;
+	
+	if(true == app->player.doPause)
+	{
+		BarUiStartEventCmd (&app->settings, "songpause",
+				app->curStation, app->playlist, &app->player, app->ph.stations,
+				PIANO_RET_OK, WAITRESS_RET_OK);
+	}
+	else
+	{
+		BarUiStartEventCmd (&app->settings, "songplay",
+				app->curStation, app->playlist, &app->player, app->ph.stations,
+				PIANO_RET_OK, WAITRESS_RET_OK);
+	}
+	
 	pthread_cond_broadcast (&app->player.pauseCond);
 	pthread_mutex_unlock (&app->player.pauseMutex);
 }
