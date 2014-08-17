4f36797fb368cc30a648ed0a313b6f4ad6cb3927
Lars-Dominik Braun <lars@6xq.net>
2014-08-17 13:01:39 +0200

    Fix volume control with ffmpeg 2.3

--- src/config.h
+++ src/config.h
@@ -17,7 +17,7 @@
 #endif
 
 /* does graph_send_command exist (ffmpeg >=2.2) */
-#if LIBAVFILTER_VERSION_MAJOR == 4 && \
+#if LIBAVFILTER_VERSION_MAJOR >= 4 && \
 		LIBAVFILTER_VERSION_MICRO >= 100
 #define HAVE_AVFILTER_GRAPH_SEND_COMMAND
 #endif
