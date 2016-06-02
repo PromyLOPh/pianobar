dadf76dc55733ec73fb11bc3ca5cc4c9eae3a24c
Lars-Dominik Braun <lars@6xq.net>
2016-06-02 08:50:32 +0200

    Back to -dev

--- src/config.h
+++ src/config.h
@@ -4,7 +4,7 @@
 /* package name */
 #define PACKAGE "pianobar"
 
-#define VERSION "2016.06.02"
+#define VERSION "2016.06.02-dev"
 
 /* glibc feature test macros, define _before_ including other files */
 #define _POSIX_C_SOURCE 200809L
