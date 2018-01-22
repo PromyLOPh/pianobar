3c273f3ec3c999321f31ac06fd27c6930afb2344
Lars-Dominik Braun <lars@6xq.net>
2018-01-22 20:02:52 +0100

    contrib: Add usage to multi.sh
    Closes #648

--- contrib/eventcmd-examples/multi.sh
+++ contrib/eventcmd-examples/multi.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 #
-# Executes all scripts located in ~/.config/pianobar/eventcmd.d/ as if they
-# were called by pianobar directly
+# Usage
+# =====
+# 
+# Set
+# 
+# 	event_command = /path/to/multi.sh
+# 
+# in pianobar’s config file. Then create the directory
+# ~/.config/pianobar/eventcmd.d/, move your eventcmd scripts there and make
+# them executable (chmod +x). They will be run in an unspecified order the same
+# way the would have been run if pianobar called them directly (i.e. using
+# event_command).
 
 STDIN=`mktemp ${TMPDIR:-/tmp}/pianobar.XXXXXX`
 cat >> $STDIN
