80d5d507c8bbb67c0f3cc067e81caa5955e68b31
Lars-Dominik Braun <lars@6xq.net>
2018-06-22 08:57:26 +0200

    Release 2018.06.22
    HAPPY 10TH BIRTHDAY PIANOBAR!
    
    It’s first public release was created on 2008-06-05. So I’m slightly
    late to the party. Sorry about that.
    
    Also, I never thought a piece of software *I* wrote would last that
    long.

--- src/config.h
+++ src/config.h
@@ -3,7 +3,7 @@
 /* package name */
 #define PACKAGE "pianobar"
 
-#define VERSION "2017.08.30-dev"
+#define VERSION "2018.06.22"
 
 /* glibc feature test macros, define _before_ including other files */
 #define _POSIX_C_SOURCE 200809L
