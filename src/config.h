9fc45b4064cc1ea1befd092d204b68cc5884b3d1
Lars-Dominik Braun <lars@6xq.net>
2019-01-25 13:17:29 +0100

    Release 2019.01.25

--- src/config.h
+++ src/config.h
@@ -3,7 +3,7 @@
 /* package name */
 #define PACKAGE "pianobar"
 
-#define VERSION "2018.06.22-dev"
+#define VERSION "2019.01.25"
 
 /* glibc feature test macros, define _before_ including other files */
 #define _POSIX_C_SOURCE 200809L
