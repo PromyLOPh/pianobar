30fb49130e5c56bfe1cf30f5944e66138d88c20f
Lars-Dominik Braun <lars@6xq.net>
2019-02-01 11:03:15 +0100

    Back to -dev

--- src/config.h
+++ src/config.h
@@ -3,7 +3,7 @@
 /* package name */
 #define PACKAGE "pianobar"
 
-#define VERSION "2019.01.25"
+#define VERSION "2019.01.25-dev"
 
 /* glibc feature test macros, define _before_ including other files */
 #define _POSIX_C_SOURCE 200809L
