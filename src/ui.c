a9f5a8b4e1cfb7326471c4b8afc26f5c60cf79b2
CoderRing28 <lpnny1@yahoo.com>
2020-06-23 18:09:06 -0500

    Add volume change event.
    Include volume in Event Command information

--- src/ui.c
+++ src/ui.c
@@ -888,6 +888,7 @@ void BarUiStartEventCmd (const BarSettings_t *settings, const char *type,
 				"wRetStr=%s\n"
 				"songDuration=%u\n"
 				"songPlayed=%u\n"
+				"volume=%i\n"
 				"rating=%i\n"
 				"detailUrl=%s\n",
 				curSong == NULL ? "" : curSong->artist,
@@ -902,6 +903,7 @@ void BarUiStartEventCmd (const BarSettings_t *settings, const char *type,
 				curl_easy_strerror (wRet),
 				songDuration,
 				songPlayed,
+				settings->volume,
 				curSong == NULL ? PIANO_RATE_NONE : curSong->rating,
 				curSong == NULL ? "" : curSong->detailUrl
 				);
