cdf00f4af12e9a5890f47455d19a57deea797248
Lars-Dominik Braun <lars@6xq.net>
2020-11-28 11:32:58 +0100

    Release 2020.11.28

--- src/config.h
+++ src/config.h
@@ -3,7 +3,7 @@
 /* package name */
 #define PACKAGE "pianobar"
 
-#define VERSION "2020.04.05-dev"
+#define VERSION "2020.11.28"
 
 /* glibc feature test macros, define _before_ including other files */
 #define _POSIX_C_SOURCE 200809L
