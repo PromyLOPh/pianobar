c16cc605265ddfe1f10439f0f4095516450c302b
Lars-Dominik Braun <lars@6xq.net>
2024-12-21 12:41:54 +0100

    Back to -dev.

--- src/config.h
+++ src/config.h
@@ -3,7 +3,7 @@
 /* package name */
 #define PACKAGE "pianobar"
 
-#define VERSION "2024.12.21"
+#define VERSION "2024.12.21-dev"
 
 /* glibc feature test macros, define _before_ including other files */
 #define _POSIX_C_SOURCE 200809L
